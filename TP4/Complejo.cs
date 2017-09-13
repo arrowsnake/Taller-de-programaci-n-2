79b2e916a9fcca0693c04828dd8d234e6fb3815e
Nazareno Sotto <emanuel_scs@hotmail.com>
2017-09-12 22:23:29 -0300

    ya ta el coso

---
+++ TP4/Complejo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace TP4
+{
+    public class Complejo
+    {
+        private double iReal;
+        private double iImaginario;
+
+        public Complejo(double pReal, double pImaginario)
+        {
+            this.iReal = pReal;
+            this.iImaginario = pImaginario;
+        }
+
+        public double Real //Solo lectura, devuelve el valor real
+        {
+            get { return this.iReal; }
+        }
+
+        public double Imaginario //solo lectura, devuelve el valor imaginario
+        {
+            get { return this.iImaginario; }
+        }
+
+        public bool EsReal() //Verdadero si es un número real, falso si no.
+        {
+            if (this.Imaginario == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool EsImaginario() //Verdadero si es un número imaginario, falso si no.
+        {
+            if (this.Real == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool EsIgual(Complejo pOtroComplejo)  //Verdadero si es igual al otro complejo, falso si no.
+        {
+            if ((this.Real == pOtroComplejo.Real) && (this.Imaginario == pOtroComplejo.Imaginario))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public double Magnitud  //devuelve la magnitud del número complejo
+        {
+            get { return Math.Sqrt(Math.Pow(this.Real, 2) + Math.Pow(this.Imaginario, 2)); }  //Cálculo de la magnitud de un número complejo
+        }
+
+        public double ArgumentoEnRadianes //devuelve el argumento en radianes.
+        {
+            get { return Math.Tan(this.Imaginario / this.Real); }
+        }
+        public double ArgumentoEnGrados //devuelve el argumento en grados.
+        {
+            get { return (this.ArgumentoEnRadianes * (180 / Math.PI)); }  //paso los radianes a grados
+        }
+        public Complejo Conjugado //devuelve el conjugado del complejo
+        {
+            get { return (new Complejo(this.Real,(this.Imaginario * (-1)) )); }
+        }
+
+        public Complejo Sumar (Complejo pOtroComplejo) //devuelve el resultado de la suma con otro complejo
+        {
+            return (new Complejo((this.Real + pOtroComplejo.Real), (this.Imaginario + pOtroComplejo.Imaginario)));  //cálculo de la suma de dos complejos
+        }
+
+        public Complejo Restar(Complejo pOtroComplejo) //devuelve el resultado de la resta con otro complejo
+        {
+            return (new Complejo((this.Real - pOtroComplejo.Real), (this.Imaginario - pOtroComplejo.Imaginario)));  //cálculo de la resta de dos complejos
+        }
+
+        public Complejo Multiplicar(Complejo pOtroComplejo)
+        {
+            return (new Complejo(((this.Real * pOtroComplejo.Real) - (this.Imaginario * pOtroComplejo.Imaginario)), ((this.Real * pOtroComplejo.Imaginario) + (this.Imaginario * pOtroComplejo.Real)))); //Cálculo de la multiplicación de dos complejos por distributiva
+        }
+
+        public Complejo Dividir(Complejo pOtroComplejo)
+        {
+            return (new Complejo(((this.Real * pOtroComplejo.Imaginario) + (this.Imaginario * pOtroComplejo.Real)) / ((Math.Pow(pOtroComplejo.Real, 2)) * (Math.Pow(pOtroComplejo.Imaginario, 2))), ((this.Imaginario * pOtroComplejo.Imaginario) - (this.Real * pOtroComplejo.Real)) / ((Math.Pow(pOtroComplejo.Real, 2)) * (Math.Pow(pOtroComplejo.Imaginario, 2))))); //cálculo de la división de dos complejos.
+        }
+    }
+}
---
+++ TP4/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //Atributos que va a usar el número complejo principal
+            double iReal;
+            double iImaginario;
+            //Atributos que va a usar el número complejo secundario
+            double iReal2;
+            double iImaginario2;
+            //Defino el número complejo secundario
+            Complejo iComplejo2;
+
+            //Creo una instancia de clase Complejo.
+            Console.WriteLine("Ingrese número complejo");
+            Console.WriteLine("Parte real:");
+            iReal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Parte Imaginaria:");
+            iImaginario = Convert.ToDouble(Console.ReadLine());
+            Complejo iComplejo = new Complejo(iReal, iImaginario);
+
+            //Menu de opciones
+            Console.WriteLine("1 - Comprobar si es un número real");
+            Console.WriteLine("2 - Comprobar si es un número imaginario");
+            Console.WriteLine("3 - Comprobar si es igual a otro número complejo");
+            Console.WriteLine("4 - Magnitud del número complejo");
+            Console.WriteLine("5 - Argumento en grados y en radianes");
+            Console.WriteLine("6 - Número complejo conjugado");
+            Console.WriteLine("7 - Sumar otro número complejo");
+            Console.WriteLine("8 - Restar otro número complejo");
+            Console.WriteLine("9 - Multiplicar otro número complejo");
+            Console.WriteLine("10 - Dividir otro número complejo");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    if (iComplejo.EsReal()) { //comprueba si se trata de un número real.
+                        Console.WriteLine("Es un número real");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es un número real");
+                    }
+                    break;
+                case 2:
+                    if (iComplejo.EsImaginario()) { //Comprueba si se trata de un número imaginario.
+                        Console.WriteLine("Es un número imaginario");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es un número imaginario");
+                    }
+                    break;
+                case 3:
+                    //Inicializo el número complejo secundario
+                    Console.WriteLine("Ingrese otro número complejo");
+                    Console.WriteLine("Parte real:");
+                    iReal2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Parte Imaginaria:");
+                    iImaginario2 = Convert.ToDouble(Console.ReadLine());
+                    iComplejo2 = new Complejo(iReal2, iImaginario2);
+
+                    if (iComplejo.EsIgual(iComplejo2)) //Compruebo si el complejo principal y el secundario son iguales.
+                    {
+                        Console.WriteLine("Los números complejos son iguales");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No son iguales");
+                    }
+                    break;
+
+                case 4:
+                    Console.Write("La magnitud es: {0}", iComplejo.Magnitud);  //Muestro la magnitud del número complejo
+
+                    break;
+                case 5:
+                    //Muestro el argumento del complejo en grados y en radianes
+                    Console.WriteLine("El argumento en grados es: {0}", iComplejo.ArgumentoEnGrados);
+                    Console.WriteLine("El argumento en radianes es: {0}", iComplejo.ArgumentoEnRadianes);
+                    break;
+                case 6:
+                    //Muestro el conjugado del número complejo
+                    Console.WriteLine("El número complejo conjugado es: {0}", MostrarComplejo(iComplejo.Conjugado));
+                    break;
+                case 7:
+                    //inicializo el complejo secundario
+                    Console.WriteLine("Ingrese otro número complejo");
+                    Console.WriteLine("Parte real:");
+                    iReal2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Parte Imaginaria:");
+                    iImaginario2 = Convert.ToDouble(Console.ReadLine());
+                    iComplejo2 = new Complejo(iReal2, iImaginario2);
+                    //realizo la suma y muestro en pantalla
+                    Console.WriteLine(MostrarComplejo(iComplejo) + " + " + MostrarComplejo(iComplejo2) + " = " + MostrarComplejo(iComplejo.Sumar(iComplejo2)));
+                    break;
+                case 8:
+                    //inicializo el complejo secundario
+                    Console.WriteLine("Ingrese otro número complejo");
+                    Console.WriteLine("Parte real:");
+                    iReal2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Parte Imaginaria:");
+                    iImaginario2 = Convert.ToDouble(Console.ReadLine());
+                    iComplejo2 = new Complejo(iReal2, iImaginario2);
+
+                    //realizo la resta y muestro en pantalla
+                    Console.WriteLine(MostrarComplejo(iComplejo) + " - " + MostrarComplejo(iComplejo2) + " = " + MostrarComplejo(iComplejo.Restar(iComplejo2)));
+                    break;
+                case 9: 
+                    //inicializo el complejo secundario
+                    Console.WriteLine("Ingrese otro número complejo");
+                    Console.WriteLine("Parte real:");
+                    iReal2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Parte Imaginaria:");
+                    iImaginario2 = Convert.ToDouble(Console.ReadLine());
+                    iComplejo2 = new Complejo(iReal2, iImaginario2);
+
+                    //Realizo la multiplicación y muestro en pantalla
+                    Console.WriteLine(MostrarComplejo(iComplejo) + " * " + MostrarComplejo(iComplejo2) + " = " + MostrarComplejo(iComplejo.Multiplicar(iComplejo2)));
+                    break;
+                case 10:
+                    //inicializo el complejo secundario
+                    Console.WriteLine("Ingrese otro número complejo");
+                    Console.WriteLine("Parte real:");
+                    iReal2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Parte Imaginaria:");
+                    iImaginario2 = Convert.ToDouble(Console.ReadLine());
+                    iComplejo2 = new Complejo(iReal2, iImaginario2);
+
+                    //realizo la división y muestro en pantalla.
+                    Console.WriteLine(MostrarComplejo(iComplejo) + " / " + MostrarComplejo(iComplejo2) + " = " + MostrarComplejo(iComplejo.Dividir(iComplejo2)));
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+        static string MostrarComplejo(Complejo pComplejo) //Un método para mostrar en string el número imaginario en el formato "a + bi" o "a - bi"
+        {
+
+            if (pComplejo.EsReal())
+            {
+                return (pComplejo.Real.ToString()); //Si es real, solo muestra el número real
+            }
+            else if (pComplejo.EsImaginario())
+            {
+                return (pComplejo.Imaginario + "i"); //Si solo es imaginario, muestra solo el número imaginario
+            }
+            else if (pComplejo.Imaginario < 0)
+            {
+                return (pComplejo.Real + " " + pComplejo.Imaginario + "i"); //imaginario negativo: (a - bi)
+            }
+            else
+            {
+                return (pComplejo.Real + " +" + pComplejo.Imaginario + "i"); //imaginario positivo: (a + bi)
+            }
+        }
+    }
+}
